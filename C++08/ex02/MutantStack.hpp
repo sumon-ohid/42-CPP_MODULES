f99dcc28b9d3ce135e88e19e726ec0da07fc8ff8
Md Ohiduzzaman Sumon <msumon@c4r6p5.42vienna.com>
2024-08-12 16:50:51 +0200

    OK

---
+++ C++08/ex02/MutantStack.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 15:19:44 by msumon            #+#    #+#             */
+/*   Updated: 2024/08/12 16:48:09 by msumon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <list>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    private:
+
+    protected:
+
+    public:
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const T &value) : std::stack<T>(value) {}
+        MutantStack(const MutantStack &copy) : std::stack<T>(copy) {}
+        virtual ~MutantStack() {}
+
+        MutantStack &operator=(const MutantStack &copy)
+        {
+            if (this != &copy)
+                std::stack<T>::operator=(copy);
+            return (*this);
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin()
+        {
+            return (std::stack<T>::c.begin());
+        }
+
+        iterator end()
+        {
+            return (std::stack<T>::c.end());
+        }
+};
---
+++ C++08/ex02/main.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 15:20:29 by msumon            #+#    #+#             */
+/*   Updated: 2024/08/12 16:42:59 by msumon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+    // Testing with MutantStack
+    std::cout << "Testing with MutantStack:" << std::endl;
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+
+    // Testing with std::list
+    std::cout << "Testing with std::list:" << std::endl;
+    std::list<int> lst;
+    lst.push_back(5);
+    lst.push_back(17);
+    std::cout << lst.back() << std::endl;
+    lst.pop_back();
+    std::cout << lst.size() << std::endl;
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+    std::list<int>::iterator lit = lst.begin();
+    std::list<int>::iterator lite = lst.end();
+    ++lit;
+    --lit;
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
+    return 0;
+}
